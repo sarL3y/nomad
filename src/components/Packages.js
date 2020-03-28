b025c1032d9d50b66902338cdeb77bbcaadbf00e
sarL3y <matt.tapper@gmail.com>
2020-03-28 16:09:35 -0700

    Style updates

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '../sass/containers/containers.scss';
+import '../sass/components/Packages.scss';
 
 function Packages() {
 	return (
@@ -9,14 +10,14 @@ function Packages() {
                 <h3>Packages</h3>
             </div>
             <div className="flex-row large-width justify-center align-center">
-                <div className="package-container-small container-margin">
-
+                <div className="package-container-small container-margin justify-center align-center">
+                    <p className="package-header package-header-text-small text-center">Basic</p>
                 </div>
                 <div className="package-container-large container-margin">
-                    
+                    <p className="package-header package-header-text-large text-center">Race</p>
                 </div>
                 <div className="package-container-small container-margin">
-                    
+                    <p className="package-header package-header-text-small text-center">Performance</p>
                 </div>
             </div>
         </div>
