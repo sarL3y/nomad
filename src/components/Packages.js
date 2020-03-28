335952d72f21e62e03da0722706e2881823bbe13
sarL3y <matt.tapper@gmail.com>
2020-03-28 16:50:26 -0700

    Style updates

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -12,12 +12,40 @@ function Packages() {
             <div className="flex-row large-width justify-center align-center">
                 <div className="package-container-small container-margin justify-center align-center">
                     <p className="package-header package-header-text-small text-center">Basic</p>
+                    <div className="flex-column container-margin">
+                        <p className="package-info">Gears</p>
+                        <p className="package-info">Brakes</p>
+                        <p className="package-info">Adjustments</p>
+                        <p className="package-info">Parts and labor</p>
+                    </div>
+                    <div className="flex-column justify-center align-center">
+                        <button className="appointment button-small">CALL US</button>
+                    </div>
                 </div>
                 <div className="package-container-large container-margin">
                     <p className="package-header package-header-text-large text-center">Race</p>
+                    <div className="flex-column container-margin">
+                        <p className="package-info">New brakes system</p>
+                        <p className="package-info">New shifting cables/housing</p>
+                        <p className="package-info">Performance Package</p>
+                        <p className="package-info">Basic Package</p>
+                        <p className="package-info">Parts and labor</p>
+                    </div>
+                    <div className="flex-column justify-center align-center">
+                        <button className="appointment button-large">CALL US</button>
+                    </div>
                 </div>
                 <div className="package-container-small container-margin">
                     <p className="package-header package-header-text-small text-center">Performance</p>
+                    <div className="flex-column container-margin">
+                        <p className="package-info">Rim truing</p>
+                        <p className="package-info">Brake pads</p>
+                        <p className="package-info">Basic Package</p>
+                        <p className="package-info">Parts and labor</p>
+                    </div>
+                    <div className="flex-column justify-center align-center">
+                        <button className="appointment button-small">CALL US</button>
+                    </div>
                 </div>
             </div>
         </div>
