702abc5b4761ea037fb5435af646584613063761
sarL3y <matt.tapper@gmail.com>
2020-03-28 17:08:43 -0700

    Style updates

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -10,8 +10,8 @@ function Packages() {
                 <h3>Packages</h3>
             </div>
             <div className="flex-row large-width justify-center align-center">
-                <div className="package-container-small container-margin justify-center align-center">
-                    <p className="package-header package-header-text-small text-center">Basic</p>
+                <div className="package-container-small container-margin justify-center align-center position-relative">
+                    <p className="package-header package-header-text-small text-center">Basic <span className="price price-small">$30</span></p>
                     <div className="flex-column container-margin">
                         <p className="package-info">Gears</p>
                         <p className="package-info">Brakes</p>
@@ -22,8 +22,8 @@ function Packages() {
                         <button className="appointment button-small">CALL US</button>
                     </div>
                 </div>
-                <div className="package-container-large container-margin">
-                    <p className="package-header package-header-text-large text-center">Race</p>
+                <div className="package-container-large container-margin position-relative">
+                    <p className="package-header package-header-text-large text-center">Race <span className="price price-large">$115</span></p>
                     <div className="flex-column container-margin">
                         <p className="package-info">New brakes system</p>
                         <p className="package-info">New shifting cables/housing</p>
@@ -35,8 +35,8 @@ function Packages() {
                         <button className="appointment button-large">CALL US</button>
                     </div>
                 </div>
-                <div className="package-container-small container-margin">
-                    <p className="package-header package-header-text-small text-center">Performance</p>
+                <div className="package-container-small container-margin position-relative">
+                    <p className="package-header package-header-text-small text-center">Performance <span className="price price-small">$70</span></p>
                     <div className="flex-column container-margin">
                         <p className="package-info">Rim truing</p>
                         <p className="package-info">Brake pads</p>
